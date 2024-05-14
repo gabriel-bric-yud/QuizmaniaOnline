e2a79b423ac7fee233dfee0f1023279e54522745
gbabyblue <114004755+gbabyblue@users.noreply.github.com>
2024-05-14 02:50:11 -0400

    Update index.js

--- public/index.js
+++ public/index.js
@@ -7,16 +7,22 @@ const bottomContainer = $('.bottomContainer');
 const topContainer = $('#topContainer')
 const question = $("#question")
 const btnMultipleChoice = $('.multipleChoice');
-const choiceContainer = $('.choiceContainer')
-const dropdownOption = $('.dropdown-menu')
-const quizNav = $('.quizNav')
+const quizContainer = $('.quizContainer');
+const flashcardContainer = $('.flashcardContainer');
+const flashcardText = $('#flashcardText');
+const flashcardBtn = $('#flashcardBtn');
+const choiceContainer = $('.choiceContainer');
+const dropdownOption = $('.dropdown-menu');
+const quizNav = $('.quizNav');
 const resultDisplay = $('#result');
 const pointsDisplay = $('#points');
-let currentQuiz = "csit210_midterm"
+let currentQuiz = "csit210_midterm";
 let currentStyle = "guessWord";
+let currentType = "quiz";
 
 let i;
 let abcdArray = ['a','b','c','d']
+let correctIndex;
 let points = 0;
 let newPoint = false;
 
@@ -96,8 +102,6 @@ function checkPoints() {
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
 async function MySQLFormDataRequest(url = "", dataArray = []) {
   let formData = new FormData();
   for (let i = 0; i < dataArray.length; i++) {
@@ -111,7 +115,7 @@ async function MySQLFormDataRequest(url = "", dataArray = []) {
   return response; 
 }
 
-function MySQLFormData(url = "", dataArray = [], index) {
+function quizSQLRequest(url = "", dataArray = [], index) {
   MySQLFormDataRequest(url, dataArray)
   .then((data) => data.json())
   .then((data) => {
@@ -129,83 +133,77 @@ function MySQLFormData(url = "", dataArray = [], index) {
   .then((data) => {$(playButton).prop('disabled', false); clearInterval(progressbarIncrease)})
 }
 
+function flashcardSQLRequest(url = "", dataArray = []) {
+  MySQLFormDataRequest(url, dataArray)
+  .then((data) => data.json())
+  .then((data) => {
+    console.log(data);
+    displayChoices(0, data, ["vocab", "definition"]);
+    $(flashcardText).css('backgroundColor', 'whitesmoke')
+  })
+  .then((data) => {$(playButton).prop('disabled', false); clearInterval(progressbarIncrease)})
+}
+
+async function requestApi(url = "", array = [] , index = 0) {
+  const response = await fetch(url)
+  array[index] = await response.json()
+  return response
+}
+
+function pokemonFetch(url = "", endpoints = [], keys = [], index = 0) {
+  let totalData = [];
+  requestApi(url+endpoints[0], totalData, 0)
+  .then((data) => requestApi(url+endpoints[1], totalData, 1))
+  .then((data) => requestApi(url+endpoints[2], totalData, 2))
+  .then((data) => requestApi(url+endpoints[3], totalData, 3))
+  .then((data) => console.log(totalData))
+  .then((data) => {
+    displayChoices(index, totalData, keys);
+    $(choiceContainer).filter(function(index, value) {
+      if ((index%2 == 0)) {
+        $(this).css('backgroundColor', 'whitesmoke')
+      }
+      else {
+        $(this).css('backgroundColor', 'white')
+      }
+    })
+  })
+  .then((data) => {$(playButton).prop('disabled', false); clearInterval(progressbarIncrease)})
+}
+
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//let csit210MidtermVocab = [...csit210Ch1Vocab, ... csit210Ch2Vocab, ...csit210Ch3Vocab, ...csit210Ch4Vocab]
-//let csit241MidtermVocab = [...csit241Ch1Vocab, ... csit241Ch2Vocab, ...csit241Ch3Vocab, ...csit241Ch4Vocab]
-
-
-function getArray(target) {
-  let testChoice;
-  switch ($(target).attr('id')) {
-    case "csit111Midterm":
-      testChoice = "csit111_midterm";
-      break;
-    case "csit111Final":
-      testChoice = "csit111_final";
-      break;
-    case "csit210Chapter1":
-      testChoice = "csit210_ch1";
-      break;
-    case "csit210Chapter2":
-      testChoice = "csit210_ch2";
-      break;
-    case "csit210Chapter3":
-      testChoice = "csit210_ch3";
-      break;
-    case "csit210Chapter4":
-      testChoice = "csit210_ch4";
-      break;
-    case "csit210Midterm":
-      testChoice = "csit210_midterm";
-      break;
-    case "csit210Chapter5":
-      testChoice = "csit210_ch5";
-      break;
-    case "csit241Chapter1":
-      testChoice = "csit241_ch1";
-      break;
-    case "csit241Chapter2":
-      testChoice = "csit241_ch2";
-      break;
-    case "csit241Chapter3":
-      testChoice = "csit241_ch3";
-      break;
-    case "csit241Chapter4":
-      testChoice = "csit241_ch4";
-      break;
-    case "csit241Midterm":
-      testChoice = "csit241_midterm";
-      break;
-    case "pokemon":
-      testChoice = "pokemon";
-      break;
-  }
-  return testChoice;
-}
 
 function addChoice(position, choice) {
   switch(position) {
     case 0:
-      $("#choiceA").removeClass('spinner-border').removeClass('text-primary').text(choice)
-      $("#choiceA").prev().show()
+      if (currentType == "quiz") {
+        $("#choiceA").removeClass('text-primary').text(choice)
+        $("#choiceA").prev().show()
+      }
+      else {
+        $("#flashcardText").removeClass('text-primary').text(choice)
+        $("#flashcardText").prev().show()
+      }
       break;
     case 1:
-      $("#choiceB").removeClass('spinner-border').removeClass('text-primary').text(choice)
+      $("#choiceB").removeClass('text-primary').text(choice)
       $("#choiceB").prev().show()
       break;
     case 2:
-      $("#choiceC").removeClass('spinner-border').removeClass('text-primary').text(choice)
+      $("#choiceC").removeClass('text-primary').text(choice)
       $("#choiceC").prev().show()
       break;
     case 3:
-      $("#choiceD").removeClass('spinner-border').removeClass('text-primary').text(choice)
+      $("#choiceD").removeClass('text-primary').text(choice)
       $("#choiceD").prev().show()
     }
 }
 
 function displayChoices(index, data, keys) {
-  for (let i = 0; i< 4; i++) {
+  console.log(data.length)
+  for (let i = 0; i< data.length; i++) {
     if (i == index) {
       //question.textContent = data[i][0]
       currentStyle == "guessWord" ? $(question).text(data[i][keys[1]]) : $(question).text(data[i][keys[0]])
@@ -220,7 +218,7 @@ let progressbar
 let currentProgress
 function increaseProgress() {
   let percent = 100 + "%" 
-  $('.progress-bar').css('width', percent)
+  $(progressbar).css('width', percent)
 }
 
 
@@ -256,62 +254,57 @@ function displayQuizName() {
   return displayTxt;
 }
 
-async function requestApi(url = "", array = [] , index = 0) {
-  const response = await fetch(url)
-  array[index] = await response.json()
-  return response
+function createQuiz() {
+  $('.quizText').html('<div class="spinner-border text-primary"></div>')
+  $(btnMultipleChoice).hide()
+  $(choiceContainer).css('backgroundColor', 'transparent')
+  correctIndex = Math.floor(Math.random() * 4)
+
+  setTimeout(() => {
+    if (currentQuiz != "pokemon") {
+      let dataArray = [["table", currentQuiz], ["type", currentType]]
+      quizSQLRequest("https://quizmania-online.azurewebsites.net", dataArray, correctIndex)
+    }
+    else {
+      pokemonFetch("https://pokeapi.co/api/v2/pokemon", ["1", "2", "3", "4"], ['abilities', "name"], correctIndex)
+    }
+  }, 300)
 }
 
+function createFlashcard() {
+  $('#flashcardText').html('<div class="spinner-border text-primary"></div>')
+  $(".flashcardBtn").hide()
+  $(flashcardText).fadeOut(0);
+  $(choiceContainer).css('backgroundColor', 'transparent')
+  let correctIndex = Math.floor(Math.random() * 4)
+  console.log(currentQuiz)
 
-
-function pokemonFetch(url = "", endpoints = [], keys = [], index = 0) {
-  let totalData = [];
-  requestApi(url+endpoints[0], totalData, 0)
-  .then((data) => requestApi(url+endpoints[1], totalData, 1))
-  .then((data) => requestApi(url+endpoints[2], totalData, 2))
-  .then((data) => requestApi(url+endpoints[3], totalData, 3))
-  .then((data) => console.log(totalData))
-  .then((data) => {
-    displayChoices(index, totalData, keys);
-    $(choiceContainer).filter(function(index, value) {
-      if ((index%2 == 0)) {
-        $(this).css('backgroundColor', 'whitesmoke')
-      }
-      else {
-        $(this).css('backgroundColor', 'white')
-      }
-    })
-  })
-  .then((data) => {$(playButton).prop('disabled', false); clearInterval(progressbarIncrease)})
+  setTimeout(() => {
+    if (currentQuiz != "pokemon") {
+      let dataArray = [["table", currentQuiz], ["type", currentType]]
+      flashcardSQLRequest("https://quizmania-online.azurewebsites.net", dataArray)
+    }
+    else {
+      pokemonFetch("https://quizmania-online.azurewebsites.net", ["pikachu/", "charmander/", "squirtle/", "bulbasaur/"], ['abilities', "name"], correctIndex)
+    }
+  }, 300)
 }
 
 function createQuestion() {
   $(resultDisplay).text(displayQuizName());
   $(playButton).prop('disabled', true)
-  $(question).text("").append('<div class="progress" style = "width: 300px; max-width: 90vw"><div class="progress-bar" style="width:5%"></div></div>')
+  $(question).text("").append('<div class="progress" style = "width: 300px; max-width: 90vw"><div class="progress-bar" style="width:5%" aria-valuenow = "0"></div></div>')
   currentProgress = 0;
   progressbar = $('.progress-bar');
   progressbarIncrease = setInterval(increaseProgress, 30)
   $('.message').remove()
-  $('.choiceText').html('<div class="spinner-border text-primary"></div>')
-  $(btnMultipleChoice).hide()
-  $(choiceContainer).css('backgroundColor', '')
-  correctIndex = Math.floor(Math.random() * 4)
-  //console.log(currentQuiz)
-
-  setTimeout(() => {
-    if (currentQuiz != "pokemon") {
-      let dataArray = [["table", currentQuiz]]
-      MySQLFormData("https://quizmaniaapi.azurewebsites.net", dataArray, correctIndex)
-    }
-    else {
-      pokemonFetch("https://quizmaniaapi.azurewebsites.net", ["pikachu/", "charmander/", "squirtle/", "bulbasaur/"], ['abilities', "name"], correctIndex)
-    }
-
-  }, 300)
-
+  if (currentType == "quiz") {
+    createQuiz();
+  }
+  else {
+    createFlashcard();
+  }
 }
-
 
 function checkAnswer(choice) {
     if ($(choice).attr("id") == abcdArray[correctIndex]) {
@@ -351,7 +344,6 @@ function clearResult() {
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-let correctIndex
 function playGame() {
   $(btnMultipleChoice).click((e) => {
     let userChoice = $(e.target);
@@ -361,29 +353,22 @@ function playGame() {
   $(".choiceText").click((e) => {
     let userChoice = $(e.target);
     let answer = $(userChoice).siblings("button:first-of-type")
+    console.log(answer)
     checkAnswer($(answer));
-  })
-    
+  })   
 }
 
-$(choiceContainer).filter(function(index, value) {
-  if ((index%2 == 0)) {
-    $(this).css('backgroundColor', 'whitesmoke')
-  }
-  else {
-    $(this).css('backgroundColor', 'white')
-  }
+
+$(flashcardBtn).click((e) => {
+  $(flashcardBtn).fadeOut(150, function() {
+    $(flashcardBtn).hide();
+    $(flashcardText).fadeIn(200);
+  })
 })
-
-
-let gameStarted = false;
-$('.quizNav').fadeOut(0);
-$(playButton).fadeOut(0);
-$('.quizNav').fadeIn(1000);
-$(playButton).fadeIn(1000);
 
 $(dropdownOption).click((e) => {
   target = $(e.target);
+  console.log(target)
   switch ($(target).attr('class')) {
     case "dropdown-item-text quizOption":
       currentQuiz = $(target).attr("id");;
@@ -391,20 +376,44 @@ $(dropdownOption).click((e) => {
     case "dropdown-item-text quizStyle":
       currentStyle = $(target).attr("id");
       break;
+    case "dropdown-item-text quizType":
+      currentType = $(target).attr("id");
+      break;
   }
 })
 
 $(playButton).click( (e) => {
-  if (gameStarted == false) {
-    //$('.dropdown').removeClass('dropdown').addClass('dropup')
-    $(topContainer).fadeOut().show().css('display', 'grid').fadeIn()
-    $(playButton).text('Random Question')
-    playGame();
-    gameStarted = true
+
+  if (currentQuiz != "pokemon") {
+    if (currentType == "quiz") {
+      $(flashcardContainer).fadeOut(20)
+      $(quizContainer).fadeIn(20)
+    }
+    else {
+      $(quizContainer).fadeOut(20)
+      $(flashcardContainer).fadeIn(20)
+    }
+    if (gameStarted == false) {
+      //$('.dropdown').removeClass('dropdown').addClass('dropup')
+      $(topContainer).fadeOut().show().css('display', 'grid').fadeIn()
+      $(playButton).text('Random Question')
+      playGame();
+      gameStarted = true
+    }
+    clearColors(); 
+    clearResult();
+    createQuestion()
   }
-  clearColors(); 
-  clearResult();
-  createQuestion()
 })
 
 $("*").on("touchend", function(e) { $(this).focus(); });
+
+
+let gameStarted = false;
+$('.quizNav').fadeOut(0);
+$(playButton).fadeOut(0);
+$('.quizNav').fadeIn(1000);
+$(playButton).fadeIn(1000);
+$(flashcardContainer).fadeOut(0);
+$(flashcardText).fadeOut(0);
+$(quizContainer).fadeOut(0);
